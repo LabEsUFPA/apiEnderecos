6b9b435886c8d4b810e869765854d1cd164acbf8
Alfredo Gabriel <amgabriel1@hotmail.com>
2023-06-10 18:19:49 -0300

    fix: test

--- src/test/java/labes/facomp/ufpa/br/api_enderecos/ApiEnderecosApplicationTests.java
+++ src/test/java/labes/facomp/ufpa/br/api_enderecos/ApiEnderecosApplicationTests.java
@@ -2,8 +2,10 @@ package labes.facomp.ufpa.br.api_enderecos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiEnderecosApplicationTests {
 
 	@Test
